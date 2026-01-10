0b8f53327b2736c319e0f5928e52e553da9965d2
Diggory Laycock <nospam@diggory.net>
2026-01-10 18:00:46 +0000

    As libnfc from packages provides a .pc file we can use a shim header in the modulemap

--- Package.swift
+++ Package.swift
@@ -14,20 +14,11 @@ let package = Package(
         
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "LibNFCSwift",
             dependencies: [
                 .byName(name: "Clibnfc"),
             ],
-//            swiftSettings: [
-//                .unsafeFlags(["-I/opt/homebrew/include"], .when(platforms: [.macOS]))
-//            ],
-//            linkerSettings: [
-//                .unsafeFlags(["-Xlinker", "-L/opt/homebrew/lib"], .when(platforms: [.macOS])),
-//            ]
-
         ),
         .systemLibrary(
             name: "Clibnfc",
---
+++ Sources/Clibnfc/shim.h
@@ -0,0 +1,8 @@
+//
+//  shim.h
+//  LibNFCSwift
+//
+//  Created by Diggory Laycock on 10/01/2026.
+//
+
+#include <nfc/nfc.h>
