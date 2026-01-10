731fc3c60806687049a676e6d57df1ddbf17aa06
Diggory Laycock <nospam@diggory.net>
2026-01-10 18:34:43 +0000

    Apt package name correction

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
             pkgConfig: "libnfc",
             providers: [
                 .brew(["libnfc"]),
-                .apt(["libnfc"])
+                .apt(["libnfc-dev"])
             ]
         )
     ]
